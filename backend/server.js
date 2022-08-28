89b2883985445d9cc329280b7cd66b8db0ff2dbe
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-08-27 20:09:59 -0400

    sockets

--- backend/server.js
+++ backend/server.js
@@ -21,16 +21,39 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
     console.log('User Connected', socket.id);
 
-    socket.on('join', (data) => {
-        socket.join(data)
-        
-        console.log(`User with ID: ${socket.id} joined room: ${data}`)
+    socket.on('join_room', (data) => {
+        socket.join(data);
+
+        const arr = Array.from(io.sockets.adapter.rooms);
+        const filtered = arr.filter((room) => !room[1].has(room[0]))[0];
+
+        if(filtered) {
+            let player = '';
+
+            if(filtered[1].size === 1)
+                player = 'X';
+            else if (filtered[1].size === 2)
+                player = 'O';
+
+            socket.emit('joined_room', { playersCount: filtered[1].size, player: player });
+            socket.to(data).emit('user_joined_room', { playersCount: filtered[1].size });
+
+            console.log(`User with ID: ${socket.id}, joined room: ${data}, as player: ${player}, players count: ${filtered[1].size}`);
+        }
     })
 
-    socket.on('click', (data) => {
-        console.log(data)
-        socket.to(data.room).emit('receive_click', data)
-    })
+    socket.on('exit_room', (data) => {
+        socket.leave(data)
+        const arr = Array.from(io.sockets.adapter.rooms);
+        const filtered = arr.filter((room) => !room[1].has(room[0]))[0];
+
+        
+        if(filtered) {
+            // Get players count from set in filtered array
+            socket.in(data).emit('exited_room', { playersCount: filtered[1].size });
+        }
+        console.log(`User with ID: ${socket.id} left room: ${data}`)
+    });
 
     socket.on('disconnect', () => {
         console.log('User Disconnected', socket.id)
--- frontend/src/pages/TikTakToe.jsx
+++ frontend/src/pages/TikTakToe.jsx
@@ -1,51 +1,120 @@
 import { useState, useEffect } from "react"
 import io from "socket.io-client"
+import './styles/TikTakToe.css'
 
 const socket = io("http://localhost:5000")
 
 const TikTakToe = () => {
-    const [counter, setCounter] = useState(0)
-    const [player, setPlayer] = useState('X');
+    const [player, setPlayer] = useState();
     const [uId, setUId] = useState('');
     const [room, setRoom] = useState('');
     const [joinedRoom, setJoinedRoom] = useState('');
+    const [playersCount, setPlayersCount] = useState(0);
+    const [startGame, setStartGame] = useState(false);
+    const [cells, setCells] = useState([
+        {id: 1, value: ''},
+        {id: 2, value: ''},
+        {id: 3, value: ''},
+        {id: 4, value: ''},
+        {id: 5, value: ''},
+        {id: 6, value: ''},
+        {id: 7, value: ''},
+        {id: 8, value: ''},
+        {id: 9, value: ''},
+    ]);
 
-    const handleClick = async (e) => {
-        const currCounter = counter + 1;
-        await socket.emit('click', { uId, player, currCounter, room });
-        setCounter(currCounter);
+    const handleClick = async (id) => {
+        setCells(cells.map(cell => {
+            if (cell.id === id) {
+                cell.value = player;
+            }
+            return cell;
+        }));
+        socket.emit('cell-clicked', {id, player, room});
+        // const currCounter = counter + 1;
+        // await socket.emit('click', { uId, player, currCounter, room });
+        // setCounter(currCounter);
     }
 
-    const join = async () => {
-        await socket.emit('join', room)
+
+    const joinRoom = async () => {
+        await socket.emit('join_room', room);
         setUId(socket.id)
         setJoinedRoom(room)
     }
 
+    const exitRoom = async () => {
+        await socket.emit('exit_room', room)
+        setUId('')
+        setJoinedRoom('')
+    }
+
     useEffect(() => {
-        socket.on('receive_click', (data) => {
-            setCounter(data.currCounter)
-        })
+        socket.on('joined_room', (data) => {
+            console.log(data)
+            setPlayer(data.player);
+            setPlayersCount(data.playersCount);
+        });
+        socket.on('user_joined_room', (data) => {
+            setPlayersCount(data.playersCount);
+        });
+
+        socket.on('exited_room', (data) => {
+            console.log(data)
+            setPlayersCount(data.playersCount);
+        });
+
     }, [socket]);
 
     return (
         <main>
-            <div className="flex pt-3">
-                <input type="text" value={room} onChange={(e) => setRoom(e.target.value)} />
-                <div className="btn ms-1" onClick={join}>
-                    Join
-                </div>
-            </div>
-            <div className="flex mt-1 flex-column">
-                <p>
-                    Current room: {joinedRoom}
-                </p>
-                <div className="flex">
-                    <div className="btn" onClick={handleClick}>
-                        {counter} Clicked
+            {joinedRoom ? (
+                <div className="game">
+                    <div className="flex mt-1 flex-column">
+                        <div className="flex justify-between">
+                            <h4 className="fs-3">
+                                Room: {joinedRoom}
+                            </h4>
+                            <div className="btn" onClick={exitRoom}>
+                                Exit Room
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ttt">
+                        {cells.map((cell, i) => (
+                            <div className={`ttt-cell${cell.value ? cell.value === 'X' ? ' ttt-cell-x' : ' ttt-cell-o' : ''}`}
+                                key={i}
+                                onClick={() => handleClick(cell.id)}
+                                style={{
+                                    ['--cell-player']: `var(--color-${player === 'X' ? '5' : '6'})`,
+                                }}
+                            >
+                                {cell.value}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex justify-center">
+                        <h4 className="fs-3">
+                            Players: {playersCount} | Player: {player}
+                        </h4>
                     </div>
                 </div>
-            </div>
+            ) : (
+                <div className="lobby">
+                    <div className="flex pt-3">
+                        <input 
+                            type="text" 
+                            value={room} 
+                            onChange={(e) => setRoom(e.target.value)} 
+                            placeholder="Enter room name"
+                            className="input-lg"
+                        />
+                        <div className="btn ms-1" onClick={joinRoom}>
+                            Enter
+                        </div>
+                    </div>
+                </div>
+            )}
         </main>
     )
 }
